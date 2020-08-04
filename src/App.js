1e1a22a6bf914fee859585ed4c0a842bd02e021b
Gustavo Amorim <gustavoamorim02@gmail.com>
2020-08-03 22:08:30 -0300

    Add BannerMain component. Show first video as main banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import initialData from './data/initial_data.json';
 import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
 
 function App() {
   return (
     <div>
       <Menu />
       
+      <BannerMain
+        videoTitle={initialData.category[0].videos[0].title}
+        url={initialData.category[0].videos[0].url}
+        videoDescription={"O que é Front-end? Trabalhando na área"}
+      />
     </div>
   );
 }
