47ede8b9a426e5d52ab6fc136799f86ab3133067
Gustavo Amorim <gustavoamorim02@gmail.com>
2020-08-03 22:10:14 -0300

    Add Carousel component. Show video categories and its content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import initialData from './data/initial_data.json';
 import Menu from './components/Menu';
 import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
 
 function App() {
   return (
@@ -13,6 +14,17 @@ function App() {
         url={initialData.category[0].videos[0].url}
         videoDescription={"O que é Front-end? Trabalhando na área"}
       />
+
+      <Carousel
+        ignoreFirstVideo
+        category={initialData.category[0]}
+      />
+
+      <Carousel category={initialData.category[1]} />
+      <Carousel category={initialData.category[2]} />
+      <Carousel category={initialData.category[3]} />
+      <Carousel category={initialData.category[4]} />
+      <Carousel category={initialData.category[5]} />
     </div>
   );
 }
